7bd4fe50fac31f3801abf52fc6106891f2b81e24
Mansilla <umansilla@avaya.com>
2024-05-01 14:34:30 -0600

    Primer commit, finalización del proyecto

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { ref, reactive, onMounted, watch } from "vue";
+import { db } from "./data/guitarras";
+import Guitarra from "./components/Guitarra.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+//Ejemplo recative
+// const state = reactive({
+//   guitarras: db,
+// });
+// console.log(state.guitarras);
+
+//Ejemplo Ref
+// const guitarras = ref(db);
+// console.log(guitarras.value);
+
+const guitarras = ref([]);
+const carrito = ref([]);
+const guitarra = ref({});
+
+watch(
+  carrito,
+  () => {
+    guardarLocalStorage();
+  },
+  {
+    deep: true,
+  }
+);
+
+onMounted(() => {
+  guitarras.value = db;
+  guitarra.value = db[3];
+  const carritoStorage = localStorage.getItem("carrito");
+  if (carritoStorage) carrito.value = JSON.parse(carritoStorage);
+});
+
+const guardarLocalStorage = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito.value));
+};
+
+const agregarCarrito = (guitarra) => {
+  const existeCarrito = carrito.value.findIndex(
+    (producto) => producto.id === guitarra.id
+  );
+  if (existeCarrito >= 0) {
+    carrito.value[existeCarrito].cantidad++;
+  } else {
+    guitarra.cantidad = 1;
+    carrito.value.push(guitarra);
+  }
+};
+
+const decrementarCantidad = (id) => {
+  const index = carrito.value.findIndex((producto) => producto.id === id);
+  if (carrito.value[index].cantidad <= 1) return;
+  carrito.value[index].cantidad--;
+};
+const incrementarCantidad = (id) => {
+  const index = carrito.value.findIndex((producto) => producto.id === id);
+  if (carrito.value[index].cantidad >= 5) return;
+  carrito.value[index].cantidad++;
+};
+
+const eliminarProducto = (id) => {
+  carrito.value = carrito.value.filter((producto) => producto.id !== id);
+};
+
+const vaciarCarrito = () => {
+  carrito.value = [];
+};
+</script>
+
+<template>
+  <Header
+    :carrito="carrito"
+    :guitarra="guitarra"
+    @incrementar-cantidad="incrementarCantidad"
+    @decrementar-cantidad="decrementarCantidad"
+    @agregar-carrito="agregarCarrito"
+    @eliminar-producto="eliminarProducto"
+    @vaciar-carrito="vaciarCarrito"
+  />
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+
+    <div class="row mt-5">
+      <!-- INICIO GUITARRA -->
+      <Guitarra
+        v-for="guitarra in guitarras"
+        :guitarra="guitarra"
+        @agregar-carrito="agregarCarrito"
+      />
+      <!-- FIN GUITARRA -->
+    </div>
+  </main>
+
+  <Footer />
+</template>
---
+++ src/components/Guitarra.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+
+const numero = ref(0);
+const props = defineProps({
+  guitarra: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits(["agregar-carrito"]);
+</script>
+
+<template>
+  <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-4">
+      <img
+        class="img-fluid"
+        v-bind:src="'/img/' + guitarra.imagen + '.jpg'"
+        :alt="'imagen guitarra ' + guitarra.nombre"
+      />
+    </div>
+    <div class="col-8">
+      <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+      <p>{{ numero }}</p>
+      <p>{{ guitarra.descripcion }}</p>
+      <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
+      <button
+        type="button"
+        class="btn btn-dark w-100"
+        @click="$emit('agregar-carrito', guitarra)"
+      >
+        Agregar al Carrito
+      </button>
+    </div>
+  </div>
+</template>
